a6d602dc76a43082918a6991de12522045e5b92b
Milena Moura <milena@debian>
2015-10-03 19:07:39 -0300

    Tentativa de threads

--- ep2.py
+++ ep2.py
@@ -3,10 +3,14 @@
 import readline
 import funcoes as f
 import struct
+import time
+import threading
+from processo import *
 from arquivos import *
 
 if __name__ == "__main__":
     prompt = raw_input("[ep2]: ").split()
+    proc = []
     mem = 'ep2.mem'
     vir = 'ep2.vir'
     
@@ -17,33 +21,35 @@ if __name__ == "__main__":
                 print "Tenho que carregar o arquivo {}.".format(prompt[1])
                 arquivo = open(prompt[1], 'r')
                 total, virtual = map(int, arquivo.readline().split())
-                
-                # Inicializa os arquivos de memoria total e virtual
-                makeEmptyBin(mem, total)
-                makeEmptyBin(vir, virtual)
 
                 # Acessa uma posicao especifica do arquivo de memoria
-                mapmem = memory_map(mem)
-                print len(mapmem)
-                print mapmem[0:10]
-                print mapmem[0]
-                mapmem[4] = chr(64)
-                mapmem[0] = chr(2)
-                mapmem[2] = chr(16)
-                mapmem.close()
+                #mapmem = memory_map(mem)
+                #print len(mapmem)
+                #print mapmem[0:10]
+                #print mapmem[0]
+                #mapmem[4] = chr(64)
+                #mapmem[0] = chr(2)
+                #mapmem[2] = chr(16)
+                #mapmem.close()
                     
-                with open(mem, 'rb') as a:
-                    print map(ord, a.read(5))
+                #with open(mem, 'rb') as a:
+                #    print map(ord, a.read(5))
                     
                 trace =  arquivo.readlines()
-                for i in xrange(len(trace)):
-                    trace[i] = trace[i].split()
-                    trace[i][0] = int(trace[i][0])
-                    trace[i][2:len(trace)] = map(int, trace[i][2:len(trace)])
-                print total, virtual
-                print trace
+                for t in trace:
+                    t = t.split()
+                    t[0] = int(t[0])
+                    t[2:len(t)] = map(int, t[2:len(t)])
+                    par = []
+                    for i in xrange(4, len(t)-2):
+                        x, y = t[i:i+2]
+                        par.append([x, y])
+                    p = Processo(t[0], t[1], t[2], t[3], par)
+                    proc.append(p)
+                #print total, virtual
+                #print trace
                 arquivo.close()
-                f.imprimeTempo(trace)
+                #f.imprimeTempo(trace)
                 
             except IOError:
                 print "Arquivo inexistente"
@@ -58,5 +64,17 @@ if __name__ == "__main__":
                     
         if prompt[0] == "executa":
             print "Vou imprimir resultados de {} em {} segundos.".format(prompt[1], prompt[1])
-        
+
+            # Inicializa os arquivos de memoria total e virtual
+            makeEmptyBin(mem, total)
+            makeEmptyBin(vir, virtual)
+            threads = []
+            inicio = time.time()
+            for p in proc:
+                t = threading.Thread(target = p.iniciaContagem(inicio))
+                threads.append(t)
+            for t in threads:
+                t.start()
+
+
         prompt = raw_input("[ep2]: ").split()
--- processo.py
+++ processo.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 import time
 
-
-
-
-
 class Processo:
 
-    def __init__(self, t0, nome, b, lista):
+    def __init__(self, t0, nome, tf, b, lista):
         self.t0 = t0
         self.nome = nome
+        self.tf = tf
         self.b = b
         self.acesso = lista
 
     def alocaEspaco(self, espaco):
-        print "Quero alocar o meu espaco de {} bytes!!!".format(espaco)
+        print "Sou o processo {}. Quero alocar o meu espaco de {} bytes!!!".format(self.nome, espaco)
 
     def lePosicao(self, posicao):
         print "Sou o processo {}. Quero ver a posicao {}".format(self.nome, posicao)
@@ -23,17 +20,20 @@ class Processo:
         espera = self.t0 - (time.time() - inicio)
         if espera > 0:
             time.sleep(espera)
-        
+        print time.time() - inicio
         self.alocaEspaco(self.b)
 
         for entry in self.acesso:
             espera = entry[1] - (time.time() - inicio)
             if espera > 0:
                 time.sleep(espera)
-        
+            print time.time()-inicio
             self.lePosicao(entry[0])
 
-
+        espera = self.tf - (time.time() - inicio)
+        if espera > 0:
+            time.sleep(espera)
+        print "O processo {} acabou. :(".format(self.nome)
 
 def main():
     proc = Processo(2, "primeiro", 4,  [[1, 2],[3, 3], [4, 5], [6, 7], [10, 8]])
