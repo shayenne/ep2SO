ea8b5a46a9fa114d8d214bad0c0b6de1e8dccb01
Milena Moura <milena@debian>
2015-10-03 18:13:56 -0300

    Rascunho classe Processo

--- processo.py
+++ processo.py
@@ -1,29 +1,44 @@
+#!/usr/bin/env python
 import time
 
-def alocaEspaco(tempo):
-    print "Passaram {} segundos, quero alocar o meu espaco!!!".format(tempo)
 
-def lePosicao(tempo, posicao):
-    print "Passaram {} segundos, quero ver a posicao {}".format(tempo, posicao)
 
-def iniciaContagem(inicio):
-    t0 = 2
-    espera = t0 - time.clock() - inicio
-    time.sleep(espera)
-    print "Esperei ", espera
-    alocaEspaco(t0)
-    a = [3, 4, 5, 6]
-    for i in a:
-        espera = i - time.clock() - inicio
-        print time.clock() - inicio
-        time.sleep(espera)
-        print time.clock() - inicio
-        print "Esperei ", espera, inicio, time.clock()
-        lePosicao(i, 20)
+
+
+class Processo:
+
+    def __init__(self, t0, nome, b, lista):
+        self.t0 = t0
+        self.nome = nome
+        self.b = b
+        self.acesso = lista
+
+    def alocaEspaco(self, espaco):
+        print "Quero alocar o meu espaco de {} bytes!!!".format(espaco)
+
+    def lePosicao(self, posicao):
+        print "Sou o processo {}. Quero ver a posicao {}".format(self.nome, posicao)
+
+    def iniciaContagem(self, inicio):
+        espera = self.t0 - (time.time() - inicio)
+        if espera > 0:
+            time.sleep(espera)
+        
+        self.alocaEspaco(self.b)
+
+        for entry in self.acesso:
+            espera = entry[1] - (time.time() - inicio)
+            if espera > 0:
+                time.sleep(espera)
+        
+            self.lePosicao(entry[0])
+
+
 
 def main():
-    ini = time.clock()
-    iniciaContagem(ini)
+    proc = Processo(2, "primeiro", 4,  [[1, 2],[3, 3], [4, 5], [6, 7], [10, 8]])
+    ini = time.time()
+    proc.iniciaContagem(ini)
 
 if __name__ == "__main__":
     main()
